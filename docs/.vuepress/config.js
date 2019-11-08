0ca487c42ab77a11cb31a79fbeca853500b56f92
coltborg <coltborg@gmail.com>
2019-11-08 10:11:26 -0600

    Add google fonts and a markdown test page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,9 @@ module.exports = {
   title: "Remote Work Encyclopedia",
   description:
     "Moonlight's tactical guide to working from anywhere! It covers getting things done, hiring great people, and growing quickly - based on proven tactics from the companies and developers in Moonlight's global community.",
+  head: [
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:400,500,600|Roboto:400,400i,500,700&display=swap' }],
+  ],
   serviceWorker: true,
   themeConfig: {
     docsDir: "docs",
@@ -19,6 +22,7 @@ module.exports = {
     repoLabel: "Contribute",
     smoothScroll: true,
     sidebar: [
+      "markdown_examples",
       "/",
       "story",
       "every_company_is_remote",
