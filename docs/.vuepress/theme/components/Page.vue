6b2a706ad5c1ada265e4f8b992da331fca56fe8c
Philip I. Thomas <philip@moonlightwork.com>
2019-11-10 10:54:58 -0500

    add copyright to footer

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -9,28 +9,47 @@
 
     <slot name="bottom" />
 
-    <footer class="footer">Make with ❤️ from <a href="https://www.moonlightwork.com/" rel="noopener noreferrer nofollow" target="_blank">Moonlight</a></footer>
+    <footer class="footer">
+      <div>
+        Make with ❤️ by
+        <a
+          href="https://www.moonlightwork.com/"
+          rel="noopener"
+          target="_blank"
+        >Moonlight</a>.
+      </div>
+      <span class="copyright">&copy; {{ new Date().getFullYear() }} Moonlight Work Inc.</span>
+    </footer>
   </main>
 </template>
 
 <script>
-import PageEdit from '@parent-theme/components/PageEdit.vue'
-import PageNav from '@parent-theme/components/PageNav.vue'
+import PageEdit from "@parent-theme/components/PageEdit.vue";
+import PageNav from "@parent-theme/components/PageNav.vue";
 
 export default {
   components: { PageEdit, PageNav },
-  props: ['sidebarItems']
-}
+  props: ["sidebarItems"]
+};
 </script>
 
 <style lang="stylus">
-$light-3 = #F6F6F6
-$dark-3 = #3D3D4B
+$light-3 = #F6F6F6;
+$dark-3 = #3D3D4B;
+$dark-5 = #92929F;
+$font-xs = 0.625rem /* 10px */;
 
-.footer
-  background-color $light-3
-  color $dark-3
-  padding 2rem
-  text-align center
-  font-size 0.875rem
+.footer {
+  background-color: $light-3;
+  color: $dark-3;
+  padding: 2rem;
+  text-align: center;
+  font-size: 0.875rem;
+}
+
+.copyright {
+  color: $dark-5;
+  padding: 1rem;
+  font-size: $font-xs;
+}
 </style>
