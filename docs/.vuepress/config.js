16d53e5f898f2581d0e2fb3887a42ac8dd3bbc69
Team Member (Moonlight) <team_member_mnlt@moonlightwork.com>
2019-11-12 10:54:02 -0600

    Add favicon and mobile logo
    I copied the Navbar component from the parent theme and then added
    logic to replace the image src on smaller devices.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,32 @@ module.exports = {
         href:
           "https://fonts.googleapis.com/css?family=Montserrat:400,500,600|Roboto:400,400i,500,700&display=swap"
       }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/assets/img/apple-touch-icon.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/assets/img/favicon32x32.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/assets/img/favicon16x16.png"
+      }
     ]
   ],
   serviceWorker: true,
@@ -18,6 +44,7 @@ module.exports = {
     editLinks: true,
     editLinkText: "Suggest an improvement",
     logo: "/assets/img/rwe-logo.svg",
+    mobileLogo: "/assets/img/rwe-logo-mobile.svg",
     nav: [
       {
         text: "Moonlight",
