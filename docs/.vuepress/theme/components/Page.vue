57e28021980a1e86d2e130ccaf8dbbe702377da2
coltborg <coltborg@gmail.com>
2019-11-14 12:29:48 -0600

    Add table of contents to bottom of intro page

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -7,6 +7,11 @@
 
     <PageNav v-bind="{ sidebarItems }" />
 
+    <div class="table-of-content" :class="{ 'hidden': hideTableOfContents }">
+      <h3 class="title">Table of Content</h3>
+      <Sidebar class="intro-sidebar" :items="sidebarItems"></Sidebar>
+    </div>
+
     <slot name="bottom" />
 
     <footer class="footer">
@@ -26,10 +31,20 @@
 <script>
 import PageEdit from "@parent-theme/components/PageEdit.vue";
 import PageNav from "@parent-theme/components/PageNav.vue";
+import Sidebar from "@parent-theme/components/Sidebar.vue";
 
 export default {
-  components: { PageEdit, PageNav },
-  props: ["sidebarItems"]
+  name: 'Page',
+
+  components: { PageEdit, PageNav, Sidebar },
+
+  props: ["sidebarItems"],
+
+  computed: {
+    hideTableOfContents() {
+      return this.$route.path === '/' ? false : true; 
+    }
+  },
 };
 </script>
 
@@ -38,6 +53,46 @@ $light-3 = #F6F6F6
 $dark-3 = #3D3D4B
 $dark-5 = #92929F
 $font-xs = 0.625rem /* 10px */
+
+.table-of-content
+  &.hidden
+    display none
+
+  .title
+    padding-top 2.875rem
+    margin 0 2rem 1rem
+    border-top 1px solid $borderColor
+
+@media (min-width: $MQMobile)
+  .table-of-content
+    display none
+
+.intro-sidebar
+  background-color #fff
+  background-color transparent
+  border-right none
+  bottom initial
+  box-sizing border-box
+  font-size 16px
+  left initial
+  margin 0
+  overflow-y auto
+  padding-top 0
+  position static
+  top initial
+  width initial
+  z-index 1
+
+  .nav-links
+    display none
+
+  .sidebar-link
+    padding 0.35rem 2rem 0.35rem 1.75rem
+
+
+@media (max-width: $MQMobile)
+  .intro-sidebar
+    transform initial
 
 .footer
   background-color $light-3
